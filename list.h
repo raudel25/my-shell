6bfb1f1421b5a0fa6bee9b594b7dab87179bd8d9
Raudel Alejandro Gomez Molina <raudel.gmolina@gmail.com>
2023-03-25 23:10:03 -0400

    list

---
+++ list.h
@@ -0,0 +1,28 @@
+#ifndef MY_SH_LIST_H
+#define MY_SH_LIST_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <values.h>
+
+typedef struct List{
+	int len;
+	unsigned capacity;
+	int* array;
+}List;
+
+struct List* createList();
+
+struct List* cloneList(struct List* original);
+
+void append(struct List* list, int value);
+
+int getValueAtIndex(struct List* list,int index);
+
+int setValueAtIndex(struct List* list,int index, int value);
+
+int removeAtIndex(struct List* list,int index);
+
+int addAtIndex(struct List* list, int index, int value);
+
+#endif //MY_SH_LIST_H
